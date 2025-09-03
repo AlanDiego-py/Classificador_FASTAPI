68adbe8edcb5291675ed1631d322d09a3ba7ed36
alan <alandiego175@gmail.com>
2025-09-03 07:07:22 -0300

    atualização

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ async def predict(
 # Rodar o aplicativo com: uvicorn main:app --reload
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
 
 
 
